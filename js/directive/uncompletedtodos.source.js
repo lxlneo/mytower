2b4f0a6a947598784c6dd63598a56c1d3921b876
Conis <Conis.yi@gmail.com>
2014-05-09 15:32:52 +0800

    增加status

--- js/directive/uncompletedtodos.source.js
+++ js/directive/uncompletedtodos.source.js
@@ -6,8 +6,15 @@ angular.module('BHF')
             templateUrl: "views/part/uncompletedtodos.html",
             restrict: "E",
             replace: true,
-            link: function ($scope) {
-               $scope.todos = ["A","B"];
+            link: function ($scope, $element) {
+                var url = 'project/1/issue'
+                var data = {
+                    status: $element.parent().attr('data-status')
+                }
+
+                API.doAction(url, data, function(data){
+                    $scope.data = data
+                })
             }
         }
     })
