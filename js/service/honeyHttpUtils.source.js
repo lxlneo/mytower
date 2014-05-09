c98f5608ffd82b412ca41824d18607c8b1b285e7
Conis <Conis.yi@gmail.com>
2014-05-09 14:21:05 +0800

    其它改动

--- js/service/honeyHttpUtils.source.js
+++ js/service/honeyHttpUtils.source.js
@@ -2,9 +2,53 @@
 
 angular.module('BHF')
     .factory('honeyHttpUtils', function ($http, $q, $timeout) {
-        var host = "/api/",
+        var root = "/api/",
             canceler;
 
+        //执行ajax操作
+        function doAction(api, data, method, cb){
+            var url = root + api
+
+            //如果method不是string，则method为get
+            if(typeof method !== 'string'){
+                cb = method
+                method = 'GET'
+            }
+
+            //restful api的id进行处理
+            if(data.id){
+                url += '/' + data.id
+                delete data.id
+            }
+
+            var config = {
+                url: url,
+                method: method || 'GET',
+                data: data
+            }
+
+            $http(config).then(function(res){
+                //以后再处理
+                switch(res.status){
+                    case 400:
+                        alert('找不到文件啦')
+                        break
+                    case 500:
+                        alert('大事不好了，服务器发生错误啦')
+                        break
+                    case 406:
+                        alert('貌似你提交的数据有点不对' + JSON.stringify(res.data))
+                        break
+                    case 401:
+                        alert('未经授权的访问')
+                        break
+                    default:
+                        //以后再考虑不同的处理
+                        cb(res.data)
+                        break
+                }
+            });
+        }
 
         function get(url, ajaxdata) {
             var _url = host + url,
