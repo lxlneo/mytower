eebb89af5e8c392bba1f79646dff1de643d65baf
lixuelong <lxlneo.g@gmail.com>
2014-05-09 09:30:23 +0800

    update

--- js/controller/index.source.js
+++ js/controller/index.source.js
@@ -4,8 +4,7 @@ angular.module('BHF')
 	.controller('C_index', function($scope, $rootScope, API) {
 		$rootScope.pageName = "首页"
 
-		/*API.getProjectsList("project").then(function(data){
-            console.log(data);
+		API.getProjectsList("project").then(function(data){
             $scope.prolist = data.items;
-        });*/
+        });
 	})
