6e2f41861337572cb708cf35f633eab3a51acbd1
lixuelong <lxlneo.g@gmail.com>
2014-05-09 11:21:27 +0800

    update addpro

--- js/directive/addproform.source.js
+++ js/directive/addproform.source.js
@@ -1,26 +1,30 @@
 'use strict'
 
 angular.module('BHF')
-    .directive('addproform', function(API){
-
+    .directive('addproform', function (API, $location) {
         return{
             templateUrl: "views/part/addproform.html",
             restrict: "E",
             replace: true,
-            link:function ($scope,$element) {
-                var pro = {
-                    "title":"",
-                    "description":"",
-                    "contact":"",
-                    "repos":""
-                }
-                 $scope.tosumbit = function(){
-                   pro.title = $scope.project-title;
-                   pro.contact = $scope.project-contact;
-                   pro.description = $scope.project-desc;
-                   pro.repos = $scope.project-repos;
-                     
-                 }  　　
+            link: function ($scope, $element) {
+                console.log(121212);
+                $scope.tosumbittext = "创建项目";
+                $scope.tosumbit = function () {
+                    $scope.tosumbit_text = "正在创建……";
+                    var pro = {}
+                    pro.title = $scope.projectname ||"";
+                    pro.contact = $scope.projectcontact ||"";
+                    pro.description = $scope.projectdesc ||"";
+                    pro.repos = $scope.projectrepos ||"";
+                    API.doAction("project", pro, "POST", function (data) {
+                        if (data.id) {
+                            $scope.tosumbit_text = "创建成功";
+                            $location.path() = "#/index";
+                        } else {
+                            $scope.tosumbit_text = "创建失败！";
+                        }
+                    });
+                }  　　
 
             }
         }
