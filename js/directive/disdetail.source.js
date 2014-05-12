88cddd5e6af6f529bf8c79e389f8c82044f33e88
lixuelong <lxlneo.g@gmail.com>
2014-05-12 16:59:50 +0800

    update

--- js/directive/disdetail.source.js
+++ js/directive/disdetail.source.js
@@ -1,39 +1,44 @@
 'use strict'
 
 angular.module('BHF')
-    .directive('disdetailform', function(API){
+    .directive('disdetailform', function (API,$rootScope) {
         return{
             templateUrl: "views/part/disdetailform.html",
             restrict: "E",
             replace: true,
-            link: function ($scope,$element) {
+            link: function ($scope, $element) {
                 $scope.project_id = $scope.router.id;
                 $scope.isShowDisForm = false;
-                $scope.canceldisc = function(){
+                $scope.canceldisc = function () {
                     $scope.isShowDisForm = false;
                     $scope.title = "";
                     //清空simditor
                     $scope.$broadcast('simditor:clean');
                 }
-                $scope.tosumbitdisc = function(){
+                $scope.tosumbitdisc = function () {
                     $scope.simditorvalue = "";
                     //获得值
                     $scope.$broadcast('simditor:getvalue');
                     var data = {};
                     data.title = $scope.title;
                     data.tag = 'project';
-                    data.contnet = $scope.simditorvalue;
-                    if("" == data.title||undefined==data.title){
+                    data.content = $scope.simditorvalue;
+                    if ("" == data.title || undefined == data.title) {
                         alert("标题必填哦~");
                         return;
                     }
-                    if("" == data.contnet||undefined==data.contnet){
+                    if ("" == data.content || undefined == data.content) {
                         alert("内容必填哦~");
                         return;
                     }
-                    var url = "project/"+$scope.project_id+"/issue"
-                    API.doAction(url,data,"POST",function(data){
-
+                    var url = "project/" + $scope.project_id + "/issue"
+                    API.doAction(url, data, "POST", function (data) {
+                        if(data.id) {
+                            $rootScope.$broadcast('discusslist:update');
+                            //清空simditor
+                            $scope.$broadcast('simditor:clean');
+                            $scope.title = "";
+                        }
                     })
                 }
             }
--- js/directive/dislist.source.js
+++ js/directive/dislist.source.js
@@ -9,9 +9,16 @@ angular.module('BHF')
             link: function ($scope, $routeParams) {
                 var api = 'project/' + $rootScope.project_id + '/discussion';
                 var data = {limit: 6}
-                API.doAction(api, data, function(data){
-                    $scope.data = data;
-                })
+                function refresh(){
+                    API.doAction(api, data, function(data){
+                        $scope.data = data;
+                        $scope.show_dis_more = data.pagination.count > 0;
+                    })
+                }
+                refresh();
+                $scope.$on("discusslist:update",function(){
+                    refresh();
+                });
             }
         }
     })
