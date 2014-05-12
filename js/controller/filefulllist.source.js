f7d60d363e142b6fa1a2f3364a25fc4101f73e2b
lixuelong <lxlneo.g@gmail.com>
2014-05-12 09:33:30 +0800

    fix filepath

---
+++ js/controller/filefulllist.source.js
@@ -0,0 +1,6 @@
+'use strict'
+
+angular.module('BHF')
+    .controller('C_file_full_list', function ($scope, $rootScope, $routeParams, API) {
+
+    })
--- js/directive/dislist.source.js
+++ js/directive/dislist.source.js
@@ -10,34 +10,9 @@ angular.module('BHF')
                 var api = 'project/' + $scope.router.id + '/comment';
                 $scope.project_id = $scope.router.id;
                 var data = {limit: 6}
-                var comm = {
-                    "id":"0",
-                    "title": "测试0",
-                    "content": "text",
-                    "tag": "",
-                    "owner": "",
-                    "creator": "integer",
-                    "status": "",
-                    "timestamp": "dateTime",
-                    "finish_time": "dataTime",
-                    "project_id": "1"
-                    },
-                    list = [];
-                for (var i = 0; i < 10; i++) {
-                    var title = "测试"+i;
-                    var obj = new Object();
-                     obj =  angular.extend(comm,{id:i,project_id:1,title:title,content:'content'+i,timestamp:new Date().toString()},obj);
-                    console.log(obj);
-                    list.push(obj)
-                }
-                var data = {}
-                data.items = list;
-                data.pagination = {record_count:100};
-                $scope.data = data;
-                console.log(data);
-                /*API.doAction(api, data, function(data){
+               API.doAction(api, data, function(data){
                     $scope.data = data;
-                })*/
+                })
             }
         }
     })
