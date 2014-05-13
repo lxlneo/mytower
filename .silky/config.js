a3b27101fb21aa02c12b85290926661964c643dd
lixuelong <lxlneo.g@gmail.com>
2014-05-13 09:08:10 +0800

    update filter

--- .silky/config.js
+++ .silky/config.js
@@ -8,7 +8,7 @@ module.exports = {
     proxy: {
         forward: {
             //定义代理转发
-            "/api": "http://127.0.0.1:14318"
+            "/api": "http://172.31.11.59:4000/"
         }
     },
     //替换掉文件名中的source
