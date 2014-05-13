d6dac0ee35ebe8d62e8527333261e07da5acad33
lxlneo <lxlneo.g@gmail.com>
2014-05-14 00:01:39 +0800

    add project del

--- js/controller/index.source.js
+++ js/controller/index.source.js
@@ -2,7 +2,21 @@
 
 angular.module('BHF')
     .controller('C_index', function ($scope, API) {
-        API.doAction("project", {}, function (data) {
-            $scope.prolist = data.items;
-        })
+        function getprolist(){
+            API.doAction("project", {}, function (data) {
+                $scope.prolist = data.items;
+            });
+        }
+        getprolist();
+
+        $scope.delproject = function($event){
+            $event.preventDefault();
+            var project_id = $($event.target).data('pid');
+            var _project_id = parseInt(project_id,10);
+            if(confirm("确认要删除这个项目吗？")){
+                API.doAction("project/"+_project_id,{},"DELETE",function(data){
+                    getprolist();
+                })
+            }
+        }
     })
---
+++ js/directive/issue/finish.source.js
@@ -0,0 +1,51 @@
+'use strict'
+
+angular.module('BHF')
+    .directive('todosaction', function (API) {
+        return{
+            templateUrl: "views/part/issue/action.html",
+            restrict: "E",
+            replace: true,
+            link: function ($scope, $element) {
+                var issue = $scope.item;
+                var status = issue.status;
+                // 判断状态 doing 正在处理中
+                $scope.showpause = status == 'doing';
+                //更新状态
+                function _update_status(status) {
+                    API.doAction('project/' + issue.project_id +'/issue/' + issue.id + '/status', {status: status}, "PUT", function (data) {
+                        $scope.$emit('issue:save');
+                    });
+                }
+
+                // 编辑
+                function _edit() {
+                    $scope.$emit('issue:showedit',issue);
+                }
+
+                // 删除
+                function _delete() {
+                    if(confirm("确认要删除吗？")){
+                        _update_status('trash');
+                    }
+                }
+
+                $scope.clickAction = function (action) {
+                    switch (action) {
+                        case 'doing':
+                            _update_status('pause');
+                            break;
+                        case 'pause':
+                            _update_status('doing');
+                            break;
+                        case 'edit':
+                            _edit();
+                            break;
+                        case 'delete':
+                            _delete();
+                            break;
+                    }
+                }
+            }
+        }
+    })
