d0f56473294733df9debc071e8d141e852dfcb4a
lixuelong <lxlneo.g@gmail.com>
2014-05-16 16:22:05 +0800

    add note

--- js/directive/issue/assigneebox.js
+++ js/directive/issue/assigneebox.js
@@ -7,23 +7,32 @@ angular.module('BHF')
             restrict: "E",
             replace: true,
             link: function ($scope, $element) {
+
+                //显示assigneebox
                 $scope.$on('assigneebox:show', function (event, data) {
                     $scope.show_assigneebox = true;
                     $scope.top = data.top;
                     $scope.left = data.left;
                 });
 
-                $scope.hideassigneebox = function(){
+                // 隐藏assigneebox
+                $scope.hideassigneebox = function () {
                     $scope.show_assigneebox = false;
                 }
-
-                $scope.set_owner_due = function(){
+                // 向后台提交数据
+                $scope.set_owner_due = function () {
 
                 }
-
+                //清除选定的信息
+                $scope.clean_assginee = function (type) {
+                    type == 'due' ? $scope.assigninfo.due = "" : $scope.assigninfo.owner = "";
+                }
+                // 责任人和预计时间
                 $scope.assigninfo = {
-                    owner:
+                    owner: "",
+                    due: ""
                 }
+
             }
         }
     })
