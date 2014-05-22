0bea62e6b8ef241b9f1048e2083203801722abf2
ec <ec.huyinghuan@gmail.com>
2014-05-22 09:20:16 +0800

    完成头像

--- js/service/USER.js
+++ js/service/USER.js
@@ -4,36 +4,43 @@ angular.module('BHF')
     .factory('USER',function ($filter,API) {
             var service = {};
             service.user = {};
-            service.refresh = function(){
+            
+            var refresh = function(){
                 API.doAction("mine", {}, function (data) {
                     service.user = data;
+                    setUserHeadImag(data);
                 });
             }
+            service.refresh = refresh;
 
-            //console.log($filter('nameToImageSrc')('h'));
-
-            var _nameToImageSrc = function(name){
-                var nameMap = {
-                    '廖正东':'1',
-                    '薛潋':'2',
-                    '李雪龙':'3',
-                    '赵子璇':'4',
-                    '周萍':'5',
-                    '兰斌':'6',
-                    '易晓峰':'7',
-                    '胡瀛寰':'8'
-                }
-                var src = nameMap[name] || 'path';
-                return src;
-            }
-
+           console.log($filter('uppercase')('h'));
+          // console.log(nameToImageSrcFilter('H'))
             service.setUser = function(data){
                 service.user = data;
-                console.log('setUser'+data.username)
-                console.log(data)
+                setUserHeadImag(data);
+               
+            }
+
+            var setUserHeadImag = function(data){
+                var _nameToImageSrc = function(name){
+                    var nameMap = {
+                        '廖正东':'1',
+                        '薛潋':'2',
+                        '李雪龙':'3',
+                        '赵子璇':'4',
+                        '周萍':'5',
+                        '兰斌':'6',
+                        '易晓峰':'7',
+                        '胡瀛寰':'8'
+                    }
+                    var src = nameMap[name] || 'path';
+                    return src;
+                }
                 var src = _nameToImageSrc(data.username);
-                console.log(src);
                 $('.account-info').find('img:first').attr('src','./images/member/@src@.png'.replace('@src@',src))
             }
+
+            refresh();
+            
             return service;
     })
