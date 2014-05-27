8797b48dd9282e0f06f0abc5c0b77f011f1188dc
ec <ec.huyinghuan@gmail.com>
2014-05-27 10:58:23 +0800

    #613#done 完成项目修改

--- js/controller/addpro.source.js
+++ js/controller/addpro.source.js
@@ -1,6 +1,32 @@
 'use strict'
 
 angular.module('BHF')
-    .controller('C_addpro', function ($scope) {
-    	console.log($scope.router.project_id)
+    .controller('C_addpro', function ($scope,$routeParams,API) {
+    	var project_id = $routeParams.project_id;
+    	console.log(project_id)
+    	//是否为项目编辑.为编辑时,project_id存在. else  is project create
+    	var isEdit =  !!project_id;
+    	$scope.isEdit = isEdit;
+    	var doEdit = function(){
+    		$scope.title = "修改项目";
+    		$scope.tosumbittext = "修改项目";
+    		API.doAction("project",{id: project_id}, "GET", function (data) {
+                console.log(data);
+                $scope.projectname = data.title;
+                $scope.projectcontact = data.contact;
+                $scope.projectrepos = data.repos;
+                $scope.projectdesc =  data.description;
+            });
+    	}
+
+    	var doCreate = function(){
+    		$scope.title = "创建新项目";
+    		$scope.tosumbittext = "修改项目";
+    	}
+
+    	if(isEdit){
+    		doEdit();
+    	}else{
+    		doCreate();
+    	}
     })
