7ff50dac289d709294a8bf94bc9a56ed1aa73422
ec <ec.huyinghuan@gmail.com>
2014-05-28 17:27:04 +0800

    #649#done 项目统计贡献度排名

--- js/controller/stat.js
+++ js/controller/stat.js
@@ -6,8 +6,17 @@ angular.module('BHF')
         $scope.project_id = $scope.router.project_id
 
         var api = 'project/' +  $scope.project_id + '/stat'
+        var dealWithContribute = function(data){
+        	if(!angular.isArray(data))return;
+        	data.forEach(function(item,index){
+        		//done * 100 + commit * 80 + 任务数 * 50 - 延误 * 70
+        		item.stat.contribute = item.stat.done*100 + item.stat.commit*80 + item.stat.total*50 - item.stat.delay*70
+        	})
+        }
         API.doAction(api, {}, function(data){
+        	dealWithContribute(data.members);
             $scope.stat = data
             $scope.today = moment().format('YYYY-MM-DD')
         })
+
     })
