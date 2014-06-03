415d3958fdc1c944e377d6785bf94cb0f19f207e
ec <ec.huyinghuan@gmail.com>
2014-06-03 10:56:03 +0800

    #0@bug#done 讨论分页

--- js/controller/disfulllist.source.js
+++ js/controller/disfulllist.source.js
@@ -4,5 +4,8 @@ angular.module('BHF')
     .controller('C_dis_full_list', function ($scope, $rootScope, $routeParams, API) {
         $scope.router = $routeParams;
         $rootScope.project_id = $scope.router.project_id;
-        $scope.$broadcast("discusslist:update",{limit:20});
+       // $scope.$broadcast("discusslist:update",{limit:20});
+        //触发 分页动作.通过事件 自定义指令无法接收
+        //pagination: {pageSize:20, offset:0, limit:20, pageIndex:1, count:9, recordCount:9, pageCount:1}
+       	$scope.C_dis_full_list_refresh ={pageSize:20,pageIndex:1}
     })
--- js/directive/dislist.source.js
+++ js/directive/dislist.source.js
@@ -9,12 +9,13 @@ angular.module('BHF')
             link: function ($scope, $routeParams) {
                 $scope.project_id = $scope.router.project_id;
                 var api = 'project/' + $scope.project_id + '/discussion';
-                var data = {limit: 6}
+                var data = {pageSize: 6}
                 function refresh(_data){
                     _data = angular.extend(data,_data);
                     API.doAction(api, _data, function(data){
                         $scope.data = data;
                         $scope.show_dis_more = data.pagination.count > 0;
+                        $scope.$emit("pagination:do",data.pagination);
                     })
                 }
                 refresh(data);
@@ -22,9 +23,19 @@ angular.module('BHF')
                     refresh(data);
                 });
 
+                $scope.$on("pagination:goto",function(e,data){
+                    refresh(data);
+                })
+
                 $scope.tohtml = function(str){
                    return $sce.trustAsHtml($(str).text().substr(0,200));
                 }
+
+                $scope.$watch("C_dis_full_list_refresh",function(newValue,OldValue,scope){
+                    if (newValue){ 
+                        refresh(newValue);
+                    }
+                });
             }
         }
     })
